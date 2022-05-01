5b5c5cfb1947e97dccb575fde1dbcce9e599bff7
ffaccia <francesco.faccia@gmail.com>
2022-05-01 22:27:52 +0200

    20220501_after_digital_ocean

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 const session = require("express-session");
 //const redis = require("redis")
 let RedisStore = require("connect-redis")(session)
